58aa35e5e0035914d230da8ca4737212e85e6898
Bruno Martins <bgcngm@gmail.com>
2024-08-27 11:12:32 +0700

    timelm: init: Delete class_main init shell script
    Apart from useless battery capacity faking, all this does is to
    start/stop data and radio services based upon the baseband type
    of several QCOM targets. Since all the supported targets in this
    common tree are of the same type, simply start all relevant services
    unconditionally and get rid of one more shell script.
    
    Change-Id: I1297b2537fcbea31d514bab5e248691cabdf523c

--- device.mk
+++ device.mk
@@ -299,7 +299,6 @@ $(call soong_config_set,lineage_recovery,bootloader_message_offset,128)
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    init.class_main.sh \
     init.mdm.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \

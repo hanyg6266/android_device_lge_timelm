c8a61deaab78bfb19c74b0897fa7ddb15692c2ea
Han Sol Jin <hansol@hansol.ca>
2025-07-14 02:55:38 -0700

    timelm: remove OP partition
    This breaks booting on Japanese model devices.
    It's also not needed.
    
    Change-Id: I0cccd571c5acb1504e4f757c9c3226c08ab219bc

--- device.mk
+++ device.mk
@@ -216,8 +216,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
-    vendor_firmware_mnt_mountpoint \
-    vendor_OP_mountpoint
+    vendor_firmware_mnt_mountpoint
 
 # Permissions
 PRODUCT_COPY_FILES += \

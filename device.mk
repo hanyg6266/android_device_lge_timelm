542834c9f40f074603b48cb1cf7239a9ad9ec6cb
pnguyen879 <howardson2001@gmail.com>
2025-08-03 22:01:50 -0500

    timelm: init: merge fstab.zram with fstab.timelm
    Change-Id: I350c336fac79d2a28cad9025ace11d4943a28edb

--- device.mk
+++ device.mk
@@ -305,7 +305,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.timelm \
     fstab.timelm.ramdisk \
-    fstab.zram \
     init.lge.rc \
     init.lge.vendor.rc \
     init.qcom.power.rc \
